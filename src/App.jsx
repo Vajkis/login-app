b76786ee2e62ad573a57081203b6c03eb6068af5
Aurimas <vajkis1@gmail.com>
2023-01-04 20:44:17 +0200

    reducer & context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useReducer } from 'react';
 import './App.scss';
+import DataContext from './components/DataContext';
 import Registration from './components/Registration';
-
-
+import data_reducer from './reducers/dataReducer';
 
 function App() {
+
+  const [data, dispachData] = useReducer(data_reducer, []);
+
   return (
-    <Registration />
+    <DataContext.Provider value={{
+      data,
+      dispachData
+    }}>
+      <Registration />
+    </DataContext.Provider>
   );
 }
 
