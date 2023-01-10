f7810a1fb0c26e2efdf14d219586ea90a192c170
Aurimas <vajkis1@gmail.com>
2023-01-10 18:44:02 +0200

    data state default value null

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import data_reducer from './reducers/dataReducer';
 
 function App() {
 
-  const [data, dispachData] = useReducer(data_reducer, []);
+  const [data, dispachData] = useReducer(data_reducer, null);
 
   return (
     <DataContext.Provider value={{
